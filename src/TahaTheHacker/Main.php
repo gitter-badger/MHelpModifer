a53b2617d00c300f3785767888fb42e1ba2ea86a
TahaTheHacker <tahasalah585@gmail.com>
2015-08-29 16:11:06 +0300

    Update Main.php

--- src/TahaTheHacker/Main.php
+++ src/TahaTheHacker/Main.php
@@ -1,6 +1,10 @@
 <?php
 
 use Pocketmine/plugin/PluginBase;
+use Pocketmine/plugin/PluginLoader;
+use Pocketmine/player;
+use Pocketmine/event/Cancellable;
+use pocketmine/item/item;
 
 class Main extends PluginBase implements Listener,CommandExecutor {
   //First part.
