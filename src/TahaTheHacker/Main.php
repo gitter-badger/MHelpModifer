c5bf0e21c3a5c5cf2c2b529abd1c77773112a803
TahaTheHacker <tahasalah585@gmail.com>
2015-08-29 20:04:46 +0300

    Update Main.php

--- src/TahaTheHacker/Main.php
+++ src/TahaTheHacker/Main.php
@@ -6,7 +6,7 @@ use pocketmine\plugin\PluginLoader;
 use pocketmine\player;
 use pocketmine\event\player\PlayerItemHeldEvent;
 use pocketmine\item\item;
-use pocketmine\event\Listenser
+use pocketmine\event\Listener
 
 class Main extends PluginBase implements Listenser {
   
