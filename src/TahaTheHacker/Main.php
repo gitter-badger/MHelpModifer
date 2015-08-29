0f65988722542421188475964dac88ae498c8630
TahaTheHacker <tahasalah585@gmail.com>
2015-08-29 16:51:54 +0300

    Update Main.php

--- src/TahaTheHacker/Main.php
+++ src/TahaTheHacker/Main.php
@@ -6,6 +6,11 @@ use Pocketmine/player;
 use Pocketmine/event/Cancellable;
 use pocketmine/item/item;
 
-class Main extends PluginBase implements Listener,CommandExecutor {
-  //First part.
+class Main extends PluginBase,player,item implements Listener,Cancellable {
+  public function ItemHeld(PlayerItemHeldEvent $event) {
+$item = $event->getItem();
+$id = $item->getId();
+$damage = $item->getDamage();
+if $getid->345 $player->sendPopup($Click_Me);
+}
 }
