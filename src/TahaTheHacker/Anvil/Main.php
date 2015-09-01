911c2aa33e824105682469479e7d13958b3578b5
TahaTheHacker <tahasalah585@gmail.com>
2015-09-01 20:57:59 +0300

    Update Main.php

--- src/TahaTheHacker/Anvil/Main.php
+++ src/TahaTheHacker/Anvil/Main.php
@@ -31,7 +31,7 @@ $player->sendTip("§l§a|§6§lSneaking §cCompass§a|");
 }
 }
 }
-if($this->hasPermission("anvil.feather")){
+if($event->hasPermission("anvil.feather")){
 $item = $event->getItem();
 if($this->getConfig()->get("feather") == "true"){
 $id = $item->getId();
