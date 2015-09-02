9f663960e3ad0f8c720452baf8f9fad49cb53499
TahaTheHacker <tahasalah585@gmail.com>
2015-09-02 21:32:25 +0300

    Update Main.php

--- src/TahaTheHacker/Anvil/Main.php
+++ src/TahaTheHacker/Anvil/Main.php
@@ -17,6 +17,7 @@ class Main extends PluginBase implements Listener {
    $this->getServer()->getPluginManager()->registerEvents($this, $this);
    $this->getLogger()->info(TextFormat::DARK_GREEN . "✔ §c+§6Anvil§c+§2 Enabled");
    	$this->saveDefaultConfig();
+   	self::addCreativeItem(Item::get(Item::276));
   }
   
   public function ItemHeld(PlayerItemHeldEvent $event) {
