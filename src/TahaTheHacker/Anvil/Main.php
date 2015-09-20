a56256fbe15b919ae5b4379b72e61d4e076af617
Taha_The_Hacker <tahasalah585@gmail.com>
2015-09-20 20:27:26 +0300

    Update Main.php

--- src/TahaTheHacker/Anvil/Main.php
+++ src/TahaTheHacker/Anvil/Main.php
@@ -75,6 +75,23 @@ public function onCmd(PlayerCommandPreprocessEvent $event){
  }
     public function onSignCreate(SignChangeEvent $event){
     	$this->getServer()->getScheduler()->scheduleRepeatingTask(new Timer($this,$event),60);
-
+    	$player = $event->getPlayer(); //Get the player
+        $line_0 = $event->getLine(0); //The sign's line 1 (Despite the number)
+        $line_1 = $event->getLine(1); //The sign's line 2
+        if(strtolower($line_0) === "[sg]" && !empty($line_1)){ //Check if the line 1 is equal to [sg] (Since strtolower) and if line 1 is not empty
+            $level = $this->getServer()->getLevelByName($line_1); //Get the level object with the name of $line_1
+            if(!$this->getServer()->isLevelGenerated($line_1)){ //Check if level is generated
+                $player->sendMessage("Failed, Not found."); //LOL
+                return false; //Return a boolean value of false
+            }
+            $player->sendMessage("Created!"); //AGAIN LOL
+            if($level instanceof Level){ //No need this but I am Bored
+                $event->setLine(0, "§c[§l§6SG§r§c]", "[" . count($level->getPlayers()) . "/10]", "§l§aTap To Join"); //Set the text. BTW, better use TextFormat::**
+                $event->setLine(1, "Map :§l§a" . $line_1);
+                $event->setLine(2, "§l§b[" . count($level->getPlayers($line_1)) . "/10]");
+                $event->setLine(3, "§l§6Tap To Join");
+            }
+        }
     }
-}
+
+    }//main
