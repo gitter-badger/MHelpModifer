7d482e42ab4ec8a4a1512bbaeea4d2c7687f6513
Taha_The_Hacker <tahasalah585@gmail.com>
2015-09-20 18:31:07 +0300

    Update Main.php

--- src/TahaTheHacker/Anvil/Main.php
+++ src/TahaTheHacker/Anvil/Main.php
@@ -85,7 +85,7 @@ public function onCmd(PlayerCommandPreprocessEvent $event){
                 $event->setLine(0, "§c[§l§6SG§r§c]", "[" . count($level->getPlayers()) . "/10]", "§l§aTap To Join"); //Set the text. BTW, better use TextFormat::**
                 $event->setLine(1, "Map :§l§a" . $line_1);
                 $event->setLine(2, "§l§b[" . count($level->getPlayers($line_1)) . "/10]");
-                $event->setLine(3, "§l§6Tap To Join")
+                $event->setLine(3, "§l§6Tap To Join");
             }
         }
     }
