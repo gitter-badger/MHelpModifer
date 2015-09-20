1728fb9ca7e54372ce29316c8a187a3594897fc0
Taha_The_Hacker <tahasalah585@gmail.com>
2015-09-20 19:33:18 +0300

    Update Main.php

--- src/TahaTheHacker/Anvil/Main.php
+++ src/TahaTheHacker/Anvil/Main.php
@@ -22,6 +22,7 @@ use pocketmine\command\CommandExecutor;
 use pocketmine\command\PluginCommand;
 use pocketmine\level\Level;
 use pocketmine\scheduler\ServerScheduler;
+use pocketmine\scheduler\PluginTask;
 
 class Main extends PluginBase implements Listener {
   
@@ -76,7 +77,7 @@ public function onCmd(PlayerCommandPreprocessEvent $event){
         $player = $event->getPlayer(); //Get the player
         $line_0 = $event->getLine(0); //The sign's line 1 (Despite the number)
         $line_1 = $event->getLine(1); //The sign's line 2
-        $this->getServer()->getScheduler()->scheduleRepeatingTask(new Timer($this,$event),1000);
+        $this->getServer()->getScheduler()->scheduleRepeatingTask(new Timer($this,$event),60);
         if(strtolower($line_0) === "[sg]" && !empty($line_1)){ //Check if the line 1 is equal to [sg] (Since strtolower) and if line 1 is not empty
             $level = $this->getServer()->getLevelByName($line_1); //Get the level object with the name of $line_1
             if(!$this->getServer()->isLevelGenerated($line_1)){ //Check if level is generated
