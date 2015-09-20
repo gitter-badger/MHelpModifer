39fcb51c18abf0d23b439bc75fc4957b04238ab0
Taha_The_Hacker <tahasalah585@gmail.com>
2015-09-21 01:21:28 +0300

    Update Timer.php

--- src/TahaTheHacker/Anvil/Timer.php
+++ src/TahaTheHacker/Anvil/Timer.php
@@ -15,15 +15,16 @@ use pocketmine\level\Position;
 class Timer extends PluginTask {
 
 
-  public function __construct(Main $plugin, Player $player) {
+  public function __construct(Main $plugin, Player $player, Event $event) {
     parent::__construct($plugin, $player);
     $this->plugin = $plugin;
     $this->player = $player;
+    $this->event = $event;
 }
   public function onRun($tick) {
-    $this->plugin->$event->setLine(0, "§c[§l§6SG§r§c]");
-    $this->plugin->$event->setLine(1, "Map :§l§a" . $line_1);
-    $this->plugin->$event->setLine(2, "§l§b[" . count($level->getPlayers($line_1)) . "/10]");
-    $this->plugin->$event->setLine(3, "§l§6Tap To Join");
+    $this->plugin->event->setLine(0, "§c[§l§6SG§r§c]");
+    $this->plugin->event->setLine(1, "Map :§l§a" . $line_1);
+    $this->plugin->event->setLine(2, "§l§b[" . count($level->getPlayers($line_1)) . "/10]");
+    $this->plugin->event->setLine(3, "§l§6Tap To Join");
 }
 }//Class
