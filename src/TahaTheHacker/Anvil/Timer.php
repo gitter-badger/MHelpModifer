1a14f6deba3c63da2d61050350fbbf00263cd090
Taha_The_Hacker <tahasalah585@gmail.com>
2015-09-21 11:27:54 +0300

    Update Timer.php

--- src/TahaTheHacker/Anvil/Timer.php
+++ src/TahaTheHacker/Anvil/Timer.php
@@ -21,9 +21,6 @@ class Timer extends PluginTask {
     $this->player = $player;
 }
   public function onRun($tick) {
-    $this->plugin->$event->setLine(0, "§c[§l§6SG§r§c]");
-    $this->plugin->$event->setLine(1, "Map :§l§a" . $line_1);
-    $this->plugin->$event->setLine(2, "§l§b[" . count($level->getPlayers($line_1)) . "/10]");
-    $this->plugin->$event->setLine(3, "§l§6Tap To Join");
+
 }
 }//Class
