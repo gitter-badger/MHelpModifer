02ed7f6da31ec249d177730e6f415bb4ee5b413c
Taha_The_Hacker <tahasalah585@gmail.com>
2015-10-06 14:19:05 +0300

    Update Main.php

--- src/TahaTheHacker/HelpModifer/Main.php
+++ src/TahaTheHacker/HelpModifer/Main.php
@@ -3,25 +3,15 @@
 namespace TahaTheHacker\HelpModifer;
 
 use pocketmine\plugin\PluginBase;
-use pocketmine\plugin\PluginLoader;
-use pocketmine\event\player\PlayerItemHeldEvent;
-use pocketmine\item\item;
-use pocketmine\tile\Sign;
-use pocketmine\level\Position;
-use pocketmine\event\block\SignChangeEvent;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerCommandPreprocessEvent;
-use pocketmine\utils\TextFormat;
 use pocketmine\command\ConsoleCommandSender;
 use pocketmine\command\CommandSender;
 use pocketmine\Player;
 use pocketmine\Server;
-use pocketmine\utils\Config;
 use pocketmine\command\Command;
 use pocketmine\command\CommandExecutor;
 use pocketmine\command\PluginCommand;
-use pocketmine\level\Level;
-use pocketmine\event\player\PlayerMoveEvent;
 
 class Main extends PluginBase implements Listener {
 	
@@ -29,6 +19,7 @@ class Main extends PluginBase implements Listener {
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 		$this->saveDefaultConfig();
 		$this->getServer()->getLogger()->info("§l§cHelp§6Modifer §aEnabled§c!");
+		$yml = yaml_parse(file_get_contents($this->getDataFolder() . "config.yml"));
 	}
 		
     
@@ -37,7 +28,7 @@ class Main extends PluginBase implements Listener {
         $player = $event->getPlayer();
 		if(strtolower($cmd[0]) === "/help" || strtolower($cmd[0]) === "/?"){
 			$event->setCancelled(true);
-			foreach($this->getConfig()->get("messages") as $msg){
+			foreach($yml["messages"] as $msg){
               	$player->sendMessage(str_replace("{player}", $player->getName(), $msg);
 			}
 		}
